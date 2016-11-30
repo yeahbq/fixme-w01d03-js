247c1389c4647d3cdbd6efa096dee6422908166c
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-11-30 14:02:25 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Can you fix all the errors in this file?
+// When it works it should render:
+// User: Neil Armstrong has been to 3 places
+
+var user {
+  name: {
+    first: 'Neil';
+    last 'Armstrong'
+  }
+  bornOn: new Date('December 12, 1979'),
+};
+
+user[occupiation] = 'spaceman';
+
+var destinations = [];
+
+destinations.push('Moon', 'Mars 'Jupiter');
+
+user.destinations = destinations;
+
+message = 'User: ' + User.name.first + ' ' + user.last + ' has been to ' + user.destination.length + ' places';
+
+document.write(message);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Fix me!
+
+This project has BUGS!
+
+Can you start up a server and fix the errors you see in the console?
