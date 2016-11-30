cd4db06c0271e36955522b0c8f999e7768c61792
Barrett Quan <21barrett@gmail.com>
2016-11-30 14:54:33 -0800

    fixed all errors

--- main.js
+++ main.js
@@ -2,22 +2,19 @@
 // When it works it should render:
 // User: Neil Armstrong has been to 3 places
 
-var user {
-  name: {
-    first: 'Neil';
-    last 'Armstrong'
-  }
-  bornOn: new Date('December 12, 1979'),
+var user = {
+  name: {first: 'Neil', last: 'Armstrong'},
+  bornOn: new Date('December 12, 1979')
 };
 
-user[occupiation] = 'spaceman';
+user['occupation'] = 'spaceman';
 
 var destinations = [];
 
-destinations.push('Moon', 'Mars 'Jupiter');
+destinations.push('Moon', 'Mars', 'Jupiter');
 
 user.destinations = destinations;
 
-message = 'User: ' + User.name.first + ' ' + user.last + ' has been to ' + user.destination.length + ' places';
+var message = 'User: ' + user.name.first + ' ' + user.name.last + ' has been to ' + user.destinations.length + ' places';
 
 document.write(message);
